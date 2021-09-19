712cb2bf331db4c93694e20086dc587a44de4729
adamjflynn <87862136+adamjflynn@users.noreply.github.com>
2021-09-19 10:56:56 -0500

    finish main html and css

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,11 @@ function startQuiz() {
     document.getElementById('start-button').remove();
     document.getElementById('header').remove();
     }
+
+// variables
+
+const question = document.getElementById('question');
+const answers = Array.from(document.getElementsByClassName('choice-text'));
+const questionNumber = document.getElementById('questionNumber');
+const numberCorrect = document.getElementById('score');
+
